764ea212bb32e980f874bb42c8bb4b6ec69da2fb
“frontIT” <zhangruinian3@gami.com>
2017-09-11 08:34:59 +0800

    init

---
+++ examples/routers/select.vue
@@ -0,0 +1,85 @@
+<!--<template>-->
+    <!--<div>-->
+        <!--{{ model10 }} - -->
+        <!--{{ model11 }}-->
+        <!--<Select v-model="model10" style="width:260px">-->
+            <!--<Option v-for="(item, index) in cityList" :value="item.value" :key="index">{{ item.label }}</Option>-->
+        <!--</Select>-->
+        <!--<Select v-model="model11" style="width:260px">-->
+            <!--<Option v-for="(item, index) in cityList" :value="item.value" :key="index">{{ item.label }}</Option>-->
+        <!--</Select>-->
+    <!--</div>-->
+<!--</template>-->
+<!--<script>-->
+    <!--export default {-->
+        <!--data () {-->
+            <!--return {-->
+                <!--cityList: [],-->
+                <!--model10: '',-->
+                <!--model11: '',-->
+                <!--model12: ''-->
+            <!--}-->
+        <!--},-->
+        <!--mounted () {-->
+            <!--setTimeout(() => {-->
+                <!--this.cityList = [-->
+                    <!--{-->
+                        <!--value: 'beijing',-->
+                        <!--label: '北京市'-->
+                    <!--},-->
+                    <!--{-->
+                        <!--value: 'shanghai',-->
+                        <!--label: '上海市'-->
+                    <!--},-->
+                    <!--{-->
+                        <!--value: 'shenzhen',-->
+                        <!--label: '深圳市'-->
+                    <!--},-->
+                    <!--{-->
+                        <!--value: 'hangzhou',-->
+                        <!--label: '杭州市'-->
+                    <!--},-->
+                    <!--{-->
+                        <!--value: 'nanjing',-->
+                        <!--label: '南京市'-->
+                    <!--},-->
+                    <!--{-->
+                        <!--value: 'chongqing',-->
+                        <!--label: '重庆市'-->
+                    <!--}-->
+                <!--];-->
+            <!--}, 1000);-->
+        <!--}-->
+    <!--}-->
+<!--</script>-->
+
+<template>
+    <div>
+        <Select v-model="value">
+            <Option v-for="item in list" :value="item.value" :label="item.label" :key="item.value"></Option>
+        </Select>
+        <Button @click="setList">set list</Button>
+    </div>
+</template>
+<script>
+    export default {
+        data () {
+            return {
+                value: '',
+                list: []
+            }
+        },
+        methods: {
+            setList () {
+                let list = [];
+                for (let i = 0; i < 400; i++) {
+                    list.push({
+                        value: 'value' + i,
+                        label: 'label' + i
+                    });
+                }
+                this.list = list;
+            }
+        }
+    }
+</script>
---
+++ examples/routers/table.vue
@@ -0,0 +1,57 @@
+<template>
+    <Table border :columns="columns4" :data="data1"></Table>
+</template>
+<script>
+    export default {
+        data () {
+            return {
+                columns4: [
+                    {
+                        type: 'selection',
+                        width: 60,
+                        align: 'center'
+                    },
+                    {
+                        title: '姓名',
+                        key: 'name'
+                    },
+                    {
+                        title: '年龄',
+                        key: 'age'
+                    },
+                    {
+                        title: '地址',
+                        key: 'address'
+                    }
+                ],
+                data1: [
+                    {
+                        name: '王小明',
+                        age: 18,
+                        address: '北京市朝阳区芍药居',
+                        _disabled: false,
+                        _checked: true
+                    },
+                    {
+                        name: '张小刚',
+                        age: 25,
+                        address: '北京市海淀区西二旗',
+                        _disabled: true
+                    },
+                    {
+                        name: '李小红',
+                        age: 30,
+                        address: '上海市浦东新区世纪大道',
+                        _disabled: true
+                    },
+                    {
+                        name: '周小伟',
+                        age: 26,
+                        address: '深圳市南山区深南大道',
+                        _disabled: true
+                    }
+                ]
+            }
+        }
+    }
+</script>
